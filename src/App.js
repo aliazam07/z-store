8366cdbc988d8054ccf2535c7251319a7f8713f1
aliazam07 <mdaliazam2002@gmail.com>
2023-09-12 21:06:34 +0530

    First commit day1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import ProductList, { Counter } from './features/counter/ProductList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <ProductList />
+    </div>
+  );
+}
+
+export default App;
